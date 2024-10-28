fb1469b4bd5b6ec10e1a6abc59db68b8106c5368
Jujuzix <maronez.juliana@gmail.com>
2024-10-28 08:32:46 -0400

    Commit 5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     "use strict";
-    if("ServiceWorker" in navigator){
+    if("serviceWorker" in navigator){
         navigator.serviceWorker.register("./sw.js");
     }
 };
